ef3fb472a6257599b11f3ea635b872361ed6354f
aintmongkol <45279127+aintmongkol@users.noreply.github.com>
2019-01-11 09:43:52 +0700

    Add files via upload

---
+++ aint.py
@@ -0,0 +1,11 @@
+print('menu')
+<<<<<<< HEAD
+print ('ข้อมูลใน ่branch dev')
+=======
+
+print ('ชาเย็น')
+print ('ชามะนาว')
+print ('adsadsadsadad')
+print ('zxczczaczczcว')
+>>>>>>> 3919dc2afa0eafb646b1606da2c67484649bc34d
+
