e9314ccece4e8f8c55f4b35db703acba944a5513
siyaphat <30380377+siyaphat@users.noreply.github.com>
2019-01-11 10:06:12 +0700

    edit by siyaphat
    today edit by tools.

--- aint.py
+++ aint.py
@@ -4,8 +4,9 @@ print ('ข้อมูลใน ่branch dev')
 =======
 
 print ('ชาเย็น')
-print ('ชามะนาว')
+
 print ('adsadsadsadad')
 print ('zxczczaczczcว')
 >>>>>>> 3919dc2afa0eafb646b1606da2c67484649bc34d
 
+print ('edit by siyaphat')
